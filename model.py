1f778a06cb97bd4edf23415e38f405bb40dc2efc
aidan.d.babb@gmail.com <aidan.d.babb@gmail.com>
2020-06-19 16:29:36 -0700

    wrapped loading of encoded sents in fxn

--- model.py
+++ model.py
@@ -4,9 +4,18 @@ import pickle
 
 tt_dir = "D:/train_test/"
 files = ["enc_en_training", "enc_en_test", "enc_is_training", "enc_is_test"]
-arrs = {}
-for filename in files:
-    with open(tt_dir+filename,"rb") as file:
-        temp = pickle.load(file)
-        arr = np.array(temp)
-        arrs[filename] = arr
+
+def get_encoded(verbose=False):
+    arrs = {}
+    for filename in files:
+        if verbose:
+            print("Loading encoded file {}".format(filename))
+        with open(tt_dir+filename,"rb") as file:
+            temp = pickle.load(file)
+            arr = np.array(temp)
+            arrs[filename] = arr
+
+    return arrs
+
+def get_angles(pos,i,d_model):
+    print
