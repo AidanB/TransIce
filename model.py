88cc14998debba719dcd423cf3488a7a859db9fa
aidan.d.babb@gmail.com <aidan.d.babb@gmail.com>
2020-06-19 14:26:31 -0700

    load encoded files, convert to np arrays

--- model.py
+++ model.py
@@ -1,2 +1,12 @@
 import tensorflow as tf
+import numpy as np
+import pickle
 
+tt_dir = "D:/train_test/"
+files = ["enc_en_training", "enc_en_test", "enc_is_training", "enc_is_test"]
+arrs = {}
+for filename in files:
+    with open(tt_dir+filename,"rb") as file:
+        temp = pickle.load(file)
+        arr = np.array(temp)
+        arrs[filename] = arr
