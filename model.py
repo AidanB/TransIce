9fef6ed72e38ea53b4545c4d4fc55248894b1f44
aidan.d.babb@gmail.com <aidan.d.babb@gmail.com>
2020-07-09 14:06:37 -0700

    working on separating out calls to main so that I can load model definition externally, because apparently TF is stupid and needs to work like that?

--- model.py
+++ model.py
@@ -360,51 +360,6 @@ class Transformer(tf.keras.Model):
 
         return final_output, attention_weights
 
-
-class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
-    def __init__(self, d_model, warmup_steps=4000):
-        super(CustomSchedule, self).__init__()
-
-        self.d_model = d_model
-        self.d_model = tf.cast(self.d_model, tf.float32)
-
-        self.warmup_steps = warmup_steps
-
-    def __call__(self, step):
-        arg1 = tf.math.rsqrt(step)
-        arg2 = step * (self.warmup_steps ** -1.5)
-
-        return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
-
-learning_rate = CustomSchedule(d_model)
-
-optimizer = tf.keras.optimizers.Adam(learning_rate, beta_1=0.9, beta_2=0.98,
-                                     epsilon=1e-9)
-
-loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
-    from_logits=True, reduction='none')
-
-
-def loss_function(real, pred):
-    mask = tf.math.logical_not(tf.math.equal(real, 0))
-    loss_ = loss_object(real, pred)
-
-    mask = tf.cast(mask, dtype=loss_.dtype)
-    loss_ *= mask
-
-    return tf.reduce_sum(loss_) / tf.reduce_sum(mask)
-
-train_loss = tf.keras.metrics.Mean(name='train_loss')
-train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(
-    name='train_accuracy')
-
-transformer = Transformer(num_layers, d_model, num_heads, dff,
-                          input_vocab_size, target_vocab_size,
-                          pe_input=input_vocab_size,
-                          pe_target=target_vocab_size,
-                          rate=dropout_rate)
-
-
 def create_masks(inp, tar):
     # Encoder padding mask
     enc_padding_mask = create_padding_mask(inp)
@@ -422,78 +377,135 @@ def create_masks(inp, tar):
 
     return enc_padding_mask, combined_mask, dec_padding_mask
 
-checkpoint_path = "C:/Users/Aidan/Documents/model checkpoints/train"
+if __name__ == "__main__":
+    class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
+        def __init__(self, d_model, warmup_steps=4000):
+            super(CustomSchedule, self).__init__()
 
-ckpt = tf.train.Checkpoint(transformer=transformer,
-                           optimizer=optimizer)
+            self.d_model = d_model
+            self.d_model = tf.cast(self.d_model, tf.float32)
 
-ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=5)
-save_model = True
+            self.warmup_steps = warmup_steps
 
-# if a checkpoint exists, restore the latest checkpoint.
-if ckpt_manager.latest_checkpoint:
-  ckpt.restore(ckpt_manager.latest_checkpoint)
-  print ('Latest checkpoint restored!!')
-  if save_model:
-      #transformer.save(tt_dir + "saved_model")
-    tf.saved_model.save(transformer,tt_dir+"saved_model")
+        def __call__(self, step):
+            arg1 = tf.math.rsqrt(step)
+            arg2 = step * (self.warmup_steps ** -1.5)
 
-# The @tf.function trace-compiles train_step into a TF graph for faster
-# execution. The function specializes to the precise shape of the argument
-# tensors. To avoid re-tracing due to the variable sequence lengths or variable
-# batch sizes (the last batch is smaller), use input_signature to specify
-# more generic shapes.
+            return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
 
-train_step_signature = [
-    tf.TensorSpec(shape=(None, None), dtype=tf.int64),
-    tf.TensorSpec(shape=(None, None), dtype=tf.int64),
-]
+    learning_rate = CustomSchedule(d_model)
+
+    optimizer = tf.keras.optimizers.Adam(learning_rate, beta_1=0.9, beta_2=0.98,
+                                         epsilon=1e-9)
+
+    loss_object = tf.keras.losses.SparseCategoricalCrossentropy(
+        from_logits=True, reduction='none')
 
 
-@tf.function(input_signature=train_step_signature)
-def train_step(inp, tar):
-    tar_inp = tar[:, :-1]
-    tar_real = tar[:, 1:]
+    def loss_function(real, pred):
+        mask = tf.math.logical_not(tf.math.equal(real, 0))
+        loss_ = loss_object(real, pred)
 
-    enc_padding_mask, combined_mask, dec_padding_mask = create_masks(inp, tar_inp)
+        mask = tf.cast(mask, dtype=loss_.dtype)
+        loss_ *= mask
 
-    with tf.GradientTape() as tape:
-        predictions, _ = transformer(inp, tar_inp,
-                                     True,
-                                     enc_padding_mask,
-                                     combined_mask,
-                                     dec_padding_mask)
-        loss = loss_function(tar_real, predictions)
+        return tf.reduce_sum(loss_) / tf.reduce_sum(mask)
 
-    gradients = tape.gradient(loss, transformer.trainable_variables)
-    optimizer.apply_gradients(zip(gradients, transformer.trainable_variables))
+    train_loss = tf.keras.metrics.Mean(name='train_loss')
+    train_accuracy = tf.keras.metrics.SparseCategoricalAccuracy(
+        name='train_accuracy')
 
-    train_loss(loss)
-    train_accuracy(tar_real, predictions)
+    transformer = Transformer(num_layers, d_model, num_heads, dff,
+                              input_vocab_size, target_vocab_size,
+                              pe_input=input_vocab_size,
+                              pe_target=target_vocab_size,
+                              rate=dropout_rate)
 
-for epoch in range(EPOCHS):
-    start = time.time()
+    checkpoint_path = "C:/Users/Aidan/Documents/model checkpoints/train"
 
-    train_loss.reset_states()
-    train_accuracy.reset_states()
+    ckpt = tf.train.Checkpoint(transformer=transformer,
+                               optimizer=optimizer)
 
-    # inp -> portuguese, tar -> english
-    for (batch, (inp, tar)) in enumerate(train_dataset):
-        train_step(inp, tar)
+    ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=5)
+    save_model = True
 
-        if batch % 50 == 0:
-            print('Epoch {} Batch {} Loss {:.4f} Accuracy {:.4f}'.format(
-                epoch + 1, batch, train_loss.result(), train_accuracy.result()))
+    # if a checkpoint exists, restore the latest checkpoint.
+    if ckpt_manager.latest_checkpoint:
+      ckpt.restore(ckpt_manager.latest_checkpoint)
+      print ('Latest checkpoint restored!!')
 
-    if (epoch + 1) % 1 == 0:
-        ckpt_save_path = ckpt_manager.save()
-        print('Saving checkpoint for epoch {} at {}'.format(epoch + 1,
-                                                            ckpt_save_path))
+      if save_model:
+          #transformer.save(tt_dir + "saved_model")
+          dummy = tf.zeros([64,39])
+          dummy_tar = tf.zeros([64,39])[:, :-1]
+          d1,d2,d3 = create_masks(dummy,dummy_tar)
+          __,__ = transformer(dummy,dummy_tar,True,d1,d2,d3)
+          tf.saved_model.save(transformer,tt_dir+"saved_model")
+          print("Model saved!")
+
+    # The @tf.function trace-compiles train_step into a TF graph for faster
+    # execution. The function specializes to the precise shape of the argument
+    # tensors. To avoid re-tracing due to the variable sequence lengths or variable
+    # batch sizes (the last batch is smaller), use input_signature to specify
+    # more generic shapes.
+
+    train_step_signature = [
+        tf.TensorSpec(shape=(None, None), dtype=tf.int64),
+        tf.TensorSpec(shape=(None, None), dtype=tf.int64),
+    ]
+
+
+    @tf.function(input_signature=train_step_signature)
+    def train_step(inp, tar):
+        tar_inp = tar[:, :-1]
+        tar_real = tar[:, 1:]
+
+        enc_padding_mask, combined_mask, dec_padding_mask = create_masks(inp, tar_inp)
+
+        with tf.GradientTape() as tape:
+            print("{0}\n{1}\n{2}\n{3}\n{4}\n".format(inp.shape,tar_inp.shape,enc_padding_mask.shape,combined_mask.shape,dec_padding_mask.shape)) # debug
+            predictions, _ = transformer(inp, tar_inp,
+                                         True,
+                                         enc_padding_mask,
+                                         combined_mask,
+                                         dec_padding_mask)
+
+            loss = loss_function(tar_real, predictions)
 
 
 
-    print('Epoch {} Loss {:.4f} Accuracy {:.4f}'.format(epoch + 1,
-                                                        train_loss.result(),
-                                                        train_accuracy.result()))
 
-    print('Time taken for 1 epoch: {} secs\n'.format(time.time() - start))
+        gradients = tape.gradient(loss, transformer.trainable_variables)
+        optimizer.apply_gradients(zip(gradients, transformer.trainable_variables))
+
+        train_loss(loss)
+        train_accuracy(tar_real, predictions)
+
+    for epoch in range(EPOCHS):
+        start = time.time()
+
+        train_loss.reset_states()
+        train_accuracy.reset_states()
+
+        # inp -> portuguese, tar -> english
+        for (batch, (inp, tar)) in enumerate(train_dataset):
+            print(inp.shape)
+            print(tar.shape)
+            train_step(inp, tar)
+
+            if batch % 50 == 0:
+                print('Epoch {} Batch {} Loss {:.4f} Accuracy {:.4f}'.format(
+                    epoch + 1, batch, train_loss.result(), train_accuracy.result()))
+
+        if (epoch + 1) % 1 == 0:
+            ckpt_save_path = ckpt_manager.save()
+            print('Saving checkpoint for epoch {} at {}'.format(epoch + 1,
+                                                                ckpt_save_path))
+
+
+
+        print('Epoch {} Loss {:.4f} Accuracy {:.4f}'.format(epoch + 1,
+                                                            train_loss.result(),
+                                                            train_accuracy.result()))
+
+        print('Time taken for 1 epoch: {} secs\n'.format(time.time() - start))
